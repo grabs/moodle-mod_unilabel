e02f48ee5a3827d9b9e0beb165e00eb3f7230823
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-08-19 15:47:28 +0200

    Change version

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'unilabeltype_accordion';
-$plugin->version   = 2022071703;
+$plugin->version   = 2022041602;
 $plugin->requires  = 2022041200;
