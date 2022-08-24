c33a86e9ea7ebe60cc372a95aaf39a7924b796bd
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-08-24 14:50:52 +0200

    MBS-6647: Compatibility with moodle 4.0 version of format_tiles

--- lib.php
+++ lib.php
@@ -150,6 +150,8 @@ function unilabel_cm_info_view(\cm_info $cm) {
     }
 
     $cm->set_content($renderer->render_from_template('mod_unilabel/content', $content));
+
+    $cm->set_custom_cmlist_item(true);
 }
 
 /**
