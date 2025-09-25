f4763ccc0b5fbd4e02e0e710645edf5d4a48d071
Andreas Grabs <info@grabs-edv.de>
2025-09-25 17:38:31 +0200

    mod_unilabel: add filter option to collapsed text

--- type/collapsedtext/lang/en/unilabeltype_collapsedtext.php
+++ type/collapsedtext/lang/en/unilabeltype_collapsedtext.php
@@ -24,14 +24,16 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$string['collapsed']            = 'Collapsed';
-$string['default_presentation'] = 'Default presentation';
-$string['dialog']               = 'Modal dialog';
-$string['edit_info']            = 'You can use this type to display a text as collapsed text. The title is used as a button to expand the text.<br>After saving, further settings can be found under the "Edit content" button.';
-$string['notitle']              = 'No title';
-$string['pluginname']           = 'Collapsed text';
-$string['pluginname_help']      = 'This content type shows you the content in a collapsed form or in a modal dialog. You have to define a title where the user can click on.';
-$string['presentation']         = 'Presentation';
-$string['privacy:metadata']     = 'The unilabel type Collapsed text does not store any personal data.';
-$string['title']                = 'Title';
-$string['useanimation']         = 'Use animation';
+$string['applytextfilters']      = 'Apply text filters';
+$string['applytextfilters_help'] = 'Note: When text filters are applied, there is a risk that the material will not be displayed correctly.';
+$string['collapsed']             = 'Collapsed';
+$string['default_presentation']  = 'Default presentation';
+$string['dialog']                = 'Modal dialog';
+$string['edit_info']             = 'You can use this type to display a text as collapsed text. The title is used as a button to expand the text.<br>After saving, further settings can be found under the "Edit content" button.';
+$string['notitle']               = 'No title';
+$string['pluginname']            = 'Collapsed text';
+$string['pluginname_help']       = 'This content type shows you the content in a collapsed form or in a modal dialog. You have to define a title where the user can click on.';
+$string['presentation']          = 'Presentation';
+$string['privacy:metadata']      = 'The unilabel type Collapsed text does not store any personal data.';
+$string['title']                 = 'Title';
+$string['useanimation']          = 'Use animation';
