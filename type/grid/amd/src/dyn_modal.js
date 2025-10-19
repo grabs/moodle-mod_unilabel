a845118687645deb0eedb6be12a7c48a78aa78b4
Andreas Grabs <moodle@grabs-edv.de>
2025-10-19 17:20:17 +0200

    mod_unilabel: add spinner to grid modal

--- type/grid/amd/src/dyn_modal.js
+++ type/grid/amd/src/dyn_modal.js
@@ -11,84 +11,98 @@ import log from 'core/log';
 import notification from 'core/notification';
 import Fragment from 'core/fragment';
 import templates from 'core/templates';
+import loadingIcon from 'core/loadingicon';
+
 
 /**
  * Initializes the modal functionality for the unilabel grid type.
  *
- * It loads the content of a grid tile by using the fragment api.
- * Each tile is loade only once.
- * @param {number} unilabelid - The ID of the unilabel instance.
+ * This function sets up event listeners for grid tiles and manages the creation
+ * and display of modals containing tile content. It uses the fragment API to
+ * load content dynamically, ensuring each tile's content is loaded only once.
+ *
+ * @param {number} unilabelid - The id of the unilabel instance.
  */
-export const init = (unilabelid) => {
-    // Object to store loaded modals, keyed by the tile id the modal is related to.
+export const init = async(unilabelid) => {
+    // Store loaded modals, keyed by the related tile id.
     const loadedModals = {};
+    const spinner = loadingIcon.getIcon();
 
-    // Add a click event listener to the body element.
+    // Add a global click event listener to the body.
     // We check later whether or not the event comes from a grid tile.
     document.querySelector('body').addEventListener('click', (event) => {
-        const dataset = event.target.dataset; // Get all dataset attributes from the event.
+        const dataset = event.target.dataset;
 
-        // Check whether the clicked element is a link from a unilabeltype_grid tile.
-        if (dataset.module != 'unilabel' || dataset.unilabelid != unilabelid || dataset.type != 'grid') {
+        // Validate if the clicked element is a unilabeltype_grid tile.
+        // This check ensures we only process relevant clicks.
+        if (dataset.module !== 'unilabel' || dataset.unilabelid != unilabelid || dataset.type !== 'grid') {
             return;
         }
 
-        // Prevent default action and stop event propagation.
+        // Prevent default link behavior and stop event bubbling.
         event.preventDefault();
         event.stopPropagation();
 
-        // Extract necessary data from the clicked tile.
-        const cmid = event.target.dataset.cmid; // The course module id.
-        const id = event.target.dataset.id; // The tile id.
-        const contextid = event.target.dataset.contextid; // The contextid.
+        // Extract tile-specific data from the clicked element.
+        const cmid = dataset.cmid; // The course module id.
+        const id = dataset.id; // The tile id.
+        const contextid = dataset.contextid; // The Context id of the unilabel instance.
 
-        // Check whether the tile has a loaded modal.
         if (loadedModals[id]) {
-            // If the modal is already loaded, show it.
-            log.debug('GRID: Show modal-' + id);
+            // If the tile related modal is already loaded, display it.
+            log.debug('GRID: Displaying existing modal for tile ' + id);
             loadedModals[id].show();
         } else {
-            // If the modal is not loaded, fetch its content and create it.
-            log.debug('GRID: Load fragment and create modal');
-            const args = {
-                type: 'grid',
-                id: id,
-                cmid: cmid,
-                contextid: contextid
-            };
-            // Load the modal content using the Fragment API.
-            Fragment.loadFragment('mod_unilabel', 'get_type_content', contextid, args).then(async(html, js) => {
-                // We use the html and the title from clicked elements dataset to create the modal.
-                const modal = await createModal(id, html, dataset.title);
-                if (js) {
-                    templates.runTemplateJS(js);
-                }
-                return modal;
-            }).catch(notification.exception);
+            // If the modal isn't loaded, create and show it.
+            log.debug('GRID: Creating new modal for tile ' + id);
+            createModal(id, cmid, contextid, dataset.title);
         }
     });
 
     /**
-     * Creates a modal with the given content and title.
-     * @param {string} id - The tile id to identify the modal later.
-     * @param {string} content - The HTML content of the modal body.
-     * @param {string} title - The title of the modal.
+     * Creates and shows a modal for a specific tile.
+     *
+     * To give the user a little feedback, we first show the modal with a spinner icon.
+     * So while the content is loading, which can take a while, the modal is already there.
+     *
+     * @param {string} id - The id of the tile.
+     * @param {string} cmid - The course module id.
+     * @param {string} contextid - The context id.
+     * @param {string} title - The title the modal is related to.
+     * @returns {Promise} A promise that resolves to the modal.
      */
-    const createModal = (id, content, title) => {
-        log.debug('GRID: Create modal width content');
-        log.debug(content);
-
-        // Create the modal using the Modal API.
-        Modal.create({
+    const createModal = async(id, cmid, contextid, title) => {
+        // Initialize the modal with a spinner icon.
+        const modal = await Modal.create({
             title: title,
-            body: content,
+            body: spinner,
             large: true,
             removeOnClose: false,
             show: true,
-        }).then((modal) => {
-            // Store the created modal in the loadedModals object.
-            loadedModals[id] = modal;
-            return true;
-        }).catch(notification.exception);
+        });
+
+        // Prepare arguments for content loading.
+        const args = {
+            type: 'grid',
+            id: id,
+            cmid: cmid,
+            contextid: contextid
+        };
+
+        // Load the fragment content and update the modal.
+        Fragment.loadFragment('mod_unilabel', 'get_type_content', contextid, args)
+            .then(async(html, js) => {
+                // Execute any JavaScript returned by the fragment api.
+                if (js) {
+                    templates.runTemplateJS(js);
+                }
+                // Update the modal body with the loaded content.
+                return modal.setBody(html);
+            })
+            .catch(notification.exception);
+
+        // Cache the modal for future use.
+        loadedModals[id] = modal;
+        return modal;
     };
 };
